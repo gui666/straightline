f519c1a555f2e044d21ee549229c01a0be731f11
gui666 <ggregorio666@hotmail.com>
2017-05-22 16:05:01 -0300

    Aula04

---
+++ src/main/java/NumExp.java
@@ -0,0 +1,35 @@
+import javaslang.Value;
+import javaslang.collection.Tree;
+
+import java.util.Map;
+
+public class NumExp extends Exp {
+   public final int num;
+
+   public NumExp(int num) {
+      this.num = num;
+   }
+
+   @Override
+   public String toString() {
+      return "NumExp{" +
+             "num=" + num +
+             '}';
+   }
+
+   @Override
+   public Tree.Node<String> toTree() {
+      return Tree.of("NumExp"+num);
+   }
+
+   @Override
+   public int maxargs() {
+      return 0;
+   }
+
+   @Override
+   public int eval(Map<String, Integer> mem) {
+
+      return num;
+   }
+}
